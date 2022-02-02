4a2df9493170c6d53ea123de88ec92fab3cebc9b
Isaiah Toliver <isaiah.toliver@gmail.com>
2022-02-02 12:52:08 -0500

    working on connection

---
+++ vue/src/components/UserPosts.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+      <input type="text"  v-model='user.id' />
+        {{this.user}}
+      
+
+  </div>
+</template>
+
+<script>
+import ServerService from '../services/ServerService.js';
+export default {
+    created(){
+        const userId = this.$route.params.userId;
+        ServerService.getUserPics(userId).then(
+            (response) => {
+                this.user = response.data;
+            }
+        )
+    }
+
+}
+</script>
+
+<style>
+
+</style>
