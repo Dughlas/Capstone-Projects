88ec34e31401ab556c1dead2649cbb20514b0a3c
Stephanie Konczal <stephaniekonczal@gmail.com>
2022-02-03 17:29:33 -0600

    added postcard.vue

---
+++ vue/src/components/Postcard.vue
@@ -0,0 +1,22 @@
+<template>
+
+<div>
+    <!-- div username
+  div image
+  div buttons
+  div comments
+  div caption -->
+</div>
+
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
