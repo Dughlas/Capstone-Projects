c5c0913a350a08c847e829b97932160d7277c769
Stephanie Konczal <stephaniekonczal@gmail.com>
2022-02-05 16:05:00 -0600

    show comment history function works, needs styling

--- vue/src/components/ViewComments.vue
+++ vue/src/components/ViewComments.vue
@@ -1,8 +1,19 @@
 <template>
   <div class="main-comment-containter">
-    <div v-for="comment in allComments" :key="comment.id">
+    <div v-for="comment in allComments.slice(0, 2)" :key="comment.id">
       <div>{{ comment.username }}</div>
       <div>{{ comment.comment }}</div>
+    </div>
+
+    <button id="comment-history-btn" v-on:click="showComments = true">
+      Show Comment History
+    </button>
+
+    <div id="show-all-comments" v-if="showComments === true">
+      <div v-for="comment in allComments.slice(2)" :key="comment.id">
+        <div>{{ comment.username }}</div>
+        <div>{{ comment.comment }}</div>
+      </div>
     </div>
   </div>
 </template>
@@ -11,14 +22,15 @@
 import ServerService from "../services/ServerService.js";
 export default {
   name: "view-comments",
-  props: ["picId"],
-
+  props: ["picId", 
+  ], 
   data() {
     return {
+      showComments: false,
       allComments: [],
       comment: {
         photoId: this.picId,
-      },
+      }
     };
   },
   created() {
@@ -27,8 +39,13 @@ export default {
       this.allComments = response.data;
     });
   },
+  methods: {
+  }
 };
 </script>
 
 <style>
+#comment-history-btn {
+  border: 2px solid black;
+}
 </style>
